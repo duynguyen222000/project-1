0f6483bfc04efe536caa8f5520c8d9db00986477
duynguyen222000 <nguyenducduy02022000@gmail.com>
2022-08-30 02:06:47 +0700

    fix

--- src/templates/Admin/AdminTemplate.jsx
+++ src/templates/Admin/AdminTemplate.jsx
@@ -14,23 +14,19 @@ export const AdminTemplate = () => {
     alert("Cần phải login");
     return <Navigate to="/login" replace={true} />;
   } else {
-    if (width < 600) {
-      return <div>Page này chưa hỗ trợ thiết bị di động</div>;
-    } else {
-      return (
-        <div id="admin">
-          <div className="container-fluid">
-            <div className="row">
-              <div className="col-2 col-lg-2 col-xs-6 col-md-6">
-                <Sidebar />
-              </div>
-              <div className="col-10 col-lg-10 col-xs-6 col-md-6">
-                <Outlet />
-              </div>
+    return (
+      <div id="admin">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-2 col-lg-2 col-xs-6 col-md-6">
+              <Sidebar />
+            </div>
+            <div className="col-10 col-lg-10 col-xs-6 col-md-6">
+              <Outlet />
             </div>
           </div>
         </div>
-      );
-    }
+      </div>
+    );
   }
 };
